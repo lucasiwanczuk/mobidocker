14924b119933f5efb3bc2119a9b66cba4b1c02c3
unknown <lucas.iwanczuk@gmail.com>
2022-11-04 10:51:28 -0300

    Modificado script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var consultaCEP = fetch('https://viacep.com.br/ws/0100100/json/')
     .then(resposta => resposta.json())
-    .then(r => console.log(r))
+    .then(r => {
+        if (r.erro) {
+            throw Error('Acho que você está perdido! Nossos exploradores não identificaram um CEP válido!')
+        } else
+            console.log(r)
+    })
     .catch(erro => console.log(erro));
-    
+
 console.log(consultaCEP);

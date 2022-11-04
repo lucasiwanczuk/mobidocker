ac61130b497eda3cf798a2f5ab4de677e6d737c3
unknown <lucas.iwanczuk@gmail.com>
2022-11-04 11:23:35 -0300

    Adicionado novo script js

---
+++ script2.js
@@ -0,0 +1,7 @@
+async function buscaEndereco() {
+    var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+    var consultaCepConvertida
+    console.log(consultaCEP)
+}
+
+buscaEndereco();

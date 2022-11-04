2ed7c26617f5d36f3e0c10813fd3c63386615338
unknown <lucas.iwanczuk@gmail.com>
2022-11-04 15:21:10 -0300

    Adicionado arquivos complementares.

--- script2.js
+++ script2.js
@@ -2,6 +2,8 @@ async function buscaEndereco() {
     try {
         var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
         var consultaCepConvertida = await consultaCEP.json();
+        if (consultaCepConvertida.erro){
+            throw Error('CEP não existe!');
         console.log(consultaCepConvertida);
     } catch (erro) {
         console.log(erro)
